0e757e7eb9f19cb77639ea9c0d0f56a63c13e0cb
Qihui <481889613@qq.com>
2025-07-24 16:16:24 +0800

    script修改

--- script/competation.sh
+++ script/competation.sh
@@ -1,7 +1,7 @@
 export CUDA_VISIBLE_DEVICES=1
 python -u ../run.py \
   --root_path ../dataset/ \
-  --data_path  CC-OV.csv \
+  --data_path  CC-PV.csv \
   --data custom \
   --freq t \
   --features MS \
@@ -13,5 +13,5 @@ python -u ../run.py \
   --input 8 \
   --batch_size 62 \
   --sampling_layers 2 \
-  --patience 3 \
+  --patience 10 \
   --use_gpu True \
